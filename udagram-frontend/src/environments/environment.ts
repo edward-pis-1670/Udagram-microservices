6bc638cc26842c47a3e778144df5a8b3bf89d74a
Edward1670 <edward.pis.1670@gmail.com>
2022-05-29 09:31:17 +0700

    final project

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a6cf11189108c43af920da26c4e33a71-117990649.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
